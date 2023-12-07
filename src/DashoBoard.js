93667dd72e54c95e62782f344ff8ecc7c9e356dd
SameerAliSk <sameeraligkl@gmail.com>
2023-12-07 09:20:11 +0530

    added graphs

--- src/DashoBoard.js
+++ src/DashoBoard.js
@@ -1,20 +1,48 @@
 import GaugeChart from "react-gauge-chart";
 import { Doughnut } from "react-chartjs-2";
 import "./Dashboard.css"
+import React from "react";
+import { ArcElement } from "chart.js";
+import { Chart as ChartJS,} from 'chart.js';
+import 'chart.js/auto'
+ChartJS.register(ArcElement);
 
-const data = {
-    labels:["Sold quantity"],
-    datasets: [
-        {
-          label: 'Avg Sold Quantity',
-          backgroundColor: 'rgba(75,192,192,1)',
-          borderColor: 'rgb(255, 99, 132)',
-          data: [65,]
-        }
-      ]
-}
 
 export default function DashBoard() {
+    const totalQuantitySold = 150; 
+    const totalOrders = 50; 
+    const expectedTargetQuantity = 200; 
+
+    
+    const completionPercentage = (totalQuantitySold / expectedTargetQuantity) * 100;
+    console.log(completionPercentage)
+
+    const data = {
+        labels: ['Avg Sold Quantity'],
+        datasets: [
+            {
+                data: [completionPercentage,100-completionPercentage],
+                backgroundColor: ['#36A2EB', '#E5E5E5'], 
+                hoverBackgroundColor: ['#36A2EB', '#E5E5E5'],
+            },
+        ],
+    };
+
+    const options = {
+        plugins: {
+            title: {
+                display: true,
+                text: `Avg Sold Quantity: ${14}`,
+                fontSize: 20,
+            },
+            legend: {
+                display: true,
+                position: 'top',
+            },
+        },
+    };
+
+      
     return(
         <div className="DashboardContainer">
             <div className="each-graph-container">
@@ -35,20 +63,8 @@ export default function DashBoard() {
             </div>
             <div className="each-graph-container">
             <h1 className="graph-heading">Avg Sold Quantity</h1>
-            <Doughnut
-            data={data}
-            options={{
-                title:{
-                  display:true,
-                  text:'Avg Sold Quantity',
-                  fontSize:20
-                },
-                legend:{
-                  display:true,
-                  position:'right'
-                }
-              }}
-            />
+            <Doughnut data={data} options={options} />
+            
             </div>
         </div>
     )
